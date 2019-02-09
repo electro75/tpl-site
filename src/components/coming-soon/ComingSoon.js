ce992299c03471c7603a7b76bb13efb8dfe152ae
Sahil Naikdhure <sanaikdhure@gmail.com>
2019-02-10 00:18:11 +0530

    adds temporary assets folder.

--- src/components/coming-soon/ComingSoon.js
+++ src/components/coming-soon/ComingSoon.js
@@ -1,17 +1,53 @@
 import React from 'react';
 
-import Typing from 'react-typing-animation';
+import Typing, {Backspace, Delay} from 'react-typing-animation';
 
 import './ComingSoon.css'
-const ComingSoon = () => {
-    return (
-        <div className="coming-soon">
-            <Typing className="texts">
-                <span>Thane Players League</span>
-            </Typing>
-        </div>
-        
-    )
-}
+export default class ComingSoon extends React.Component {
 
-export default ComingSoon
+    state = {showSlogan : false}
+
+    showSlogan() {
+        this.setState({showSlogan: true})
+    }
+
+    renderComingSoon() {
+        return (
+            <div>
+                Coming Soon ...
+            </div>
+        )
+    }
+
+    renderSlogan() {
+
+        return (
+            <div>
+                <span>Fun. </span> 
+                <Delay ms={500}/>
+                <span>Fitness. </span> 
+                <Delay ms={500}/>
+                <span>Football.</span>
+                <Delay ms={1000}/>
+                { this.renderComingSoon() }
+            </div>
+        )
+        
+    }
+
+    render() {
+        return (
+            <div className="coming-soon">
+                <Typing className="texts"
+                        startDelay={500}>
+                    <span>Thane Players League</span>
+                    <br/>
+                    <Delay ms={500}/>
+                    {this.renderSlogan()}
+                </Typing>
+            </div>
+            
+        )
+    }
+    
+}
