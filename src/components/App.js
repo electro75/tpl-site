dd1ff5cc69e3b915c1f324ae1cb84c113449c328
Sahil Naikdhure <sanaikdhure@gmail.com>
2019-02-09 13:15:38 +0530

    kick off commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ComingSoon from './ComingSoon'
+
+const App = () => {
+    return (
+        <div>
+            <ComingSoon />
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/ComingSoon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Typing from 'react-typing-animation';
+
+const ComingSoon = () => {
+    return (
+        <Typing>
+            <span>Coming Soon ...</span>
+        </Typing>
+    )
+}
+
+export default ComingSoon
